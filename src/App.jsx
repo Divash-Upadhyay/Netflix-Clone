3a24d662eb9d68ee612a33601473d861fb3316de
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-07-23 13:59:02 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import { Home } from "./Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Home />
+      <img
+        width="100%"
+        src="https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHVwcHklMjBkb2d8ZW58MHx8MHx8&w=1000&q=80"
+        alt=""
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "../Components/Navbar/Navbar";
+import "./Home.scss";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
